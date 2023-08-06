655ad9ce714c5a9d9206938df061c9b0219bc6e7
Harsh <harshdeshpremi@gmail.com>
2023-08-06 15:38:11 +0530

    Create 5_input.py

---
+++ 5_input.py
@@ -0,0 +1,15 @@
+a=input()
+print(a)
+
+b=input("enter the name")
+print(b)
+
+c=input()
+print("your name is ",c)
+
+
+# most common problem if you want to add two numbers
+x=input("enter the first number")
+y=input("enter the Second number")
+print(x+y)         #this will concatinate two inteager not add them because it act as a string
+print(int(x)+int(y))  #by typecasting we add them
