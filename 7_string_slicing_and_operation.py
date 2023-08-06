4344bb54158ee179aef49febda759282c229e01a
Harsh <harshdeshpremi@gmail.com>
2023-08-06 16:15:08 +0530

    Create 7_string_slicing_and_operation.py
    About string Slicing and some operation

---
+++ 7_string_slicing_and_operation.py
@@ -0,0 +1,18 @@
+name="hello, world"
+print(name[0:5]) 
+
+# if you find the length of the string then use this function
+print(len(name))
+
+# if you want starting two character
+print(name[0:3])
+        #  or
+print(name[:3])
+
+# negative slicing
+
+print(name[0:-3])
+
+
+nm ="harry"
+print(nm[-4:-2])
