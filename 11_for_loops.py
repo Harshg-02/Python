690894a5cb11ac6f5db01d273987a417bd2babed
Harsh <harshdeshpremi@gmail.com>
2023-08-09 23:20:43 +0530

    Create 11_for_loops.py
    About for loops

---
+++ 11_for_loops.py
@@ -0,0 +1,14 @@
+name ="hello"
+for i in name:
+    print(i, end=" ")
+
+
+color=["blue", "green ", "yellow"]
+for i in color:
+    print(i)
+
+for  k in range(5):
+    print(k)
+
+for  k in range(1,9):  #this will print value from 1 to 8
+    print(k)
