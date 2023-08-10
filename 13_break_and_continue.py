d76b1c70bd63e17ea486684ef83314ba62ffb8a5
Harsh <harshdeshpremi@gmail.com>
2023-08-10 23:16:32 +0530

    Create 13_break_and_continue.py
    About break and continue statement

---
+++ 13_break_and_continue.py
@@ -0,0 +1,18 @@
+for i in range(12):
+    if(i==10):
+        break   #if i = 10 the it get out of the loop
+    print("5 X ",i+1,"=",5*(i+1))
+
+print("out of the loop")
+
+
+
+
+#continue
+#it get out of the itertion
+for i in range(12):
+    if(i==10):
+        continue
+    print("5 X ",i,"=",5*i)
+
+print("out of the loop")
