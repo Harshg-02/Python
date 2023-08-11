ac8bf799d96686e78ddf8d01c2a7c62ab0972133
Harsh <harshdeshpremi@gmail.com>
2023-08-11 22:34:06 +0530

    Create 14_Function.py

---
+++ 14_Function.py
@@ -0,0 +1,21 @@
+ #A function is a block of code that perform a particular task
+# 1. Built-in function   (ex:- min(),max(),typr(),range())
+# 2. User-define function
+
+#Syntax --
+
+#        def function_name(parameters):
+            #code and statements
+
+def calculateGmean(a,b):     #you can create function by using def keyword
+    mean =(a*b)/(a+b)
+    print(mean)
+
+a=3
+b=5
+calculateGmean(a,b)  #function call
+
+
+
+def isgreate(a,b):
+    pass           #we you (pass) when we don't want to declare function or want to declare after some time
