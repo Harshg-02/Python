0873962a3fc2287bd5df07511818ac5e08d8ed3c
Harsh <harshdeshpremi@gmail.com>
2023-08-20 22:44:57 +0530

    Create 24_Sets.py
    sets in python

---
+++ 24_Sets.py
@@ -0,0 +1,13 @@
+# sets are unorder collection of data items
+# Sets do not contain duplicate items
+
+s={2,4,2,8}
+print(s)
+
+h={}
+print(type(h))   # it show class dictonary
+
+h=set()
+print(type(h))    # now it show class set
+
+# you can access set using for loop
