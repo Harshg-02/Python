2a11440e45a3e85a942050d09d406e18e6fa9496
Harsh <harshdeshpremi@gmail.com>
2023-10-16 19:53:58 +0530

    Create 32_enumerate_function.py

---
+++ 32_enumerate_function.py
@@ -0,0 +1,15 @@
+marks=[1,53,24,6,97,56,36,8]
+
+
+# index =0
+# for mark in marks:
+#     print(mark)
+#     if(index==3):
+#         print("awesome")
+#     index=index+1    
+
+for index,mark in enumerate(marks):
+    print(mark)
+    if(index==3):
+        print("awesome")
+   
